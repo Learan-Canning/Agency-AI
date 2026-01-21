7890c1ed616edc63397e7741cbb7ac68f3ba14ae
Learan-Canning <learancanning@mail.com>
2026-01-21 20:31:27 +0000

    Set up navigation bar with dark mode support

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const App = () => {
+
+  const [theme, setTheme] = useState('light');
+
+  return (
+    <div className='dark:bg-black relative'> 
+      <Navbar theme={theme} setTheme={setTheme} />
+      </div>
+  )
+}
+
+export default App;
